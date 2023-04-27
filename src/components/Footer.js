09a1c0a1d25af23bf68f15038d17bc6c9e3b7c61
Jakob Skote <jakob@untold.garden>
2023-04-27 23:29:18 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,12 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
+        <a href="https://postkodstiftelsen.se/" target="_blank" rel="noreferrer">
         <img className="logo1" src="/images/postkodsstiftelsen.webp" alt="postkodsstiftelsen" />
+        </a>
+        <a href="https://www.sverigeskonstforeningar.nu/" target="_blank" rel="noreferrer">
         <img className="logo2" src="/images/sverigeskonstforeningar.webp" alt="sverigeskonstforeningar" />
+        </a>
         <p className="footer-text">Projektet Vävda rum är initierat av Riksförbundet Sveriges Konstföreningar och finansieras av Postkodstiftelsen.</p>
       </div>
     </footer>
