e7b0a16333e9ee7d3d1444d3ec775f077a85600e
Jakob Skote <jakob@untold.garden>
2023-05-08 10:10:43 +0200

    updated artworks

--- src/pages/Artists/OscarHaggstrom.js
+++ src/pages/Artists/OscarHaggstrom.js
@@ -7,6 +7,10 @@ const OscarHaggstrom = () => {
         <h1>Oscar Häggström</h1><br></br>
       </div>
       <div className="text-container">
+      <p><strong>Oscar Häggström, <em>Jogg your mind</em>, 2023</strong></p>
+<p>I installationer av föremål, animationer, ljud, musik och VR-teknik skapar Oscar Häggström, med hjälp av figurer och symboler från populärkulturen, en bildvärld där allt befinner sig i en oändlig loop. Loopen ger honom möjlighet att framför en åskådare förstärka intensiteten i de situationer han har iscensatt. I rörliga bilder målar han upp vardagliga situationer och berättar historier för att försöka närma sig, analysera och förstå vår samtid. Genom att observera och lyfta fram den teknik som finns runt omkring oss vill han göra vår tid begriplig.</p>
+<p>I verket <em>Jogg your mind</em>, som tar inspiration kring frågor om optimering och objektifiering som förekommer i träningskultur som "fitspo" och "fitspiration", kommer besökaren på plats kunna träffa, träna och interagera med en pt-karaktär i appen. Hur länge du tränar tillsammans med karaktären dokumenteras och loggas och visas för alla andra besökare i en topplista. Den som har loggat mest tid efter utställningen är över kanske får ett pris.</p>
+<p>Oscar Häggström är intresserad av den diffusa gränsen mellan våra känslor och de verktyg vi använder oss av för att uttrycka dem. I sina verk ställer han därför frågor om hur vi använder moderna verktyg för att forma våra känslor och hur de formar oss, samt hur vi kan se den suddiga gräns mellan våra känslor och de verktyg vi använder för att uttrycka dem.</p>
       </div>
     </div>
   );
