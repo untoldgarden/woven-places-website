09be969291e56489e05b76a88bcef539a75e7749
Jakob Skote <jakob@untold.garden>
2023-05-08 09:48:37 +0200

    Update EricMagassa.js

--- src/pages/Artists/EricMagassa.js
+++ src/pages/Artists/EricMagassa.js
@@ -7,6 +7,12 @@ const EricMagassa = () => {
         <h1>Eric Magassa</h1><br></br>
       </div>
       <div className="text-container">
+        <p><strong>Eric Magassa, <em>Parallella världar</em>, 2023</strong></p>
+        <p>Medverkande poeter: Sandy Harry Ceesay, Nilofar Haghighi, Masoud Vatankhah</p>
+        <br></br>
+        <p>Eric Magassa är en multidisciplinär konstnär vars praktik rör sig fritt mellan olika tekniker, material och metoder. Återkommande är kopplingen till färgen, det abstrakta och det figurativa, varför han själv främst ser sig som målare. I sitt konstnärskap undersöker han teman som identitet, utanförskap, mellanförskap och historieskrivning och utforskar vad det innebär att resa mellan skilda platser och hemhörigheter. Flera arbeten tar sin utgångspunkt i de egna rötterna i Sverige, Frankrike och Senegal. Genom att arbeta med masker, mönster och montage iscensätter Magassa en rad förskjutningar mellan synligt och osynligt och mellan olika identiteter och betydelser.</p>
+        <p>Det nya verket <em>Parallella världar</em> visas vid brunnar runt omkring i Sverige. Varje plats har en själ och sin unika historia. I brunnen bor andeväsen med historier att berätta, komprimerade i ord och poesi. Med hjälp av din röst aktiverar du en magisk portal, som öppnar upp till en parallell värld. Ta ton, sjung en sång eller viska en hemlighet till den förtrollade brunnen och öppna upp en passage och färdas till en annan plats.</p>
+        <p>Eric Magassa samarbetar i det nya verket med Qalam, ett skrivarkollektiv som utgår från tanken att skrivande och språk är viktiga i processen för att avkolonisera sinnena och samhället. För Qalam är det centralt att det egna skrivandet ska få bidra till ett gemensamt utforskande och lärande genom att få ohörda röster att skriva ännu inte berättade historier.</p>
       </div>
     </div>
   );
