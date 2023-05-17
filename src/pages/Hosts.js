255ac9fb420c30372c6e0015089d65b8ca4f1a18
Jakob Skote <jakob@untold.garden>
2023-05-17 13:07:16 +0200

    added map to hosts

--- src/pages/Hosts.js
+++ src/pages/Hosts.js
@@ -9,12 +9,18 @@ const Hosts = () => {
   return (
     <div className="container">
       <div className="content-section">
-      <div className="title">
-        <ReactMarkdown>{data.hostsTitle[language]}</ReactMarkdown>
-      </div>
-      <div className="text-container">
-        <ReactMarkdown>{data.hostsText[language]}</ReactMarkdown>
-      </div>
+        <div className="title">
+          <ReactMarkdown>{data.hostsTitle[language]}</ReactMarkdown>
+        </div>
+      
+        <div className="text-container">
+          <ReactMarkdown>{data.hostsText[language]}</ReactMarkdown>
+        </div>
+        <div className="map-container">
+          <div dangerouslySetInnerHTML={{ __html: `
+            <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1oVypZQL6qjo8jc1i4V_PQgM2Eg55M4Q&ehbc=2E312F" width="640" height="480"></iframe>
+          ` }} />
+        </div>
       </div>
     </div>
   );
