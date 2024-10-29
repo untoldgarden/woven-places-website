1e3b6bfa2ce139d9053fad3c1fc6379638d7748b
Jakob Skote <jakobskote@hotmail.com>
2024-10-29 11:15:57 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -44,6 +44,18 @@ const Home = () => {
               title="Vimeo video player"
             ></iframe>
           </div>
+                 <br></br>
+          <div className="video-container">
+            <iframe 
+              src="https://player.vimeo.com/video/845219902" 
+              width="640" 
+              height="360" 
+              frameborder="0" 
+              allow="autoplay; fullscreen; picture-in-picture" 
+              allowfullscreen
+              title="Vimeo video player"
+            ></iframe>
+          </div>
         </div>
       </div>
     </div>
